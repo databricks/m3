7b306c4ab9b2e7a16040a129aeb032aa1dd02edd
HC Zhu (DB) <hc.zhu@databricks.com>
2024-04-30 13:59:51 -0700

    double check tenants

--- src/query/storage/promremote/storage.go
+++ src/query/storage/promremote/storage.go
@@ -26,16 +26,17 @@ import (
 	"encoding/base64"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"sync"
 	"time"
 
 	"github.com/m3db/m3/src/query/block"
+	"github.com/m3db/m3/src/query/models"
 	"github.com/m3db/m3/src/query/storage"
 	"github.com/m3db/m3/src/query/storage/m3/consolidators"
 	"github.com/m3db/m3/src/query/storage/m3/storagemetadata"
+	"github.com/m3db/m3/src/query/ts"
 	xerrors "github.com/m3db/m3/src/x/errors"
 	"github.com/m3db/m3/src/x/instrument"
 	xhttp "github.com/m3db/m3/src/x/net/http"
@@ -160,8 +161,9 @@ func NewStorage(opts Options) (storage.Storage, error) {
 		scope:           scope,
 		droppedWrites:   scope.Counter("dropped_writes"),
 		dupWrites:       scope.Counter("duplicate_writes"),
+		ingestorWrites:  scope.Counter("ingestor_writes"),
 		enqueued:        scope.Counter("enqueued"),
-		enqueuedAggr:    scope.Counter("enqueued_aggregated_samples"),
+		enqueueErr:      scope.Counter("enqueue_error"),
 		batchWrite:      scope.Counter("batch_write"),
 		batchWriteErr:   scope.Counter("batch_write_err"),
 		tickWrite:       scope.Counter("tick_write"),
@@ -172,6 +174,7 @@ func NewStorage(opts Options) (storage.Storage, error) {
 		noTenantFound:   scope.Counter("no_tenant_found"),
 		cancel:          cancel,
 		writeLoopDone:   make(chan struct{}),
+		wrongTenant:     scope.Counter("wrong_tenant"),
 	}
 	s.startAsync(ctx)
 	opts.logger.Info("Prometheus remote write storage created", zap.Int("num_tenants", len(queriesWithFixedTenants)))
@@ -186,8 +189,9 @@ type promStorage struct {
 	scope           tally.Scope
 	droppedWrites   tally.Counter
 	enqueued        tally.Counter
-	enqueuedAggr    tally.Counter
+	enqueueErr      tally.Counter
 	dupWrites       tally.Counter
+	ingestorWrites  tally.Counter
 	batchWrite      tally.Counter
 	batchWriteErr   tally.Counter
 	tickWrite       tally.Counter
@@ -198,6 +202,7 @@ type promStorage struct {
 	noTenantFound   tally.Counter
 	cancel          context.CancelFunc
 	writeLoopDone   chan struct{}
+	wrongTenant     tally.Counter
 }
 
 type tenantKey string
@@ -302,19 +307,65 @@ func (p *promStorage) startAsync(_ context.Context) {
 	}()
 }
 
+func deepCopy(queryOpt storage.WriteQueryOptions) storage.WriteQueryOptions {
+	// Only need Tags and DataPoints for writing to remote Prom. Other field are not used.
+	// getTenant() only uses Tags.Tags.
+	// See src/query/storage/promremote/query_coverter.go
+	// Unit is copied to pass the validation in NewWriteQuery()
+	// FromIngestor is used for logging only.
+	cp := storage.WriteQueryOptions{
+		Unit: queryOpt.Unit,
+		Tags: models.Tags{
+			Opts: queryOpt.Tags.Opts,
+		},
+		FromIngestor: queryOpt.FromIngestor,
+	}
+	cp.Datapoints = make([]ts.Datapoint, 0, len(queryOpt.Datapoints))
+	cp.Datapoints = append(cp.Datapoints, queryOpt.Datapoints...)
+
+	cp.Tags.Tags = make([]models.Tag, 0, len(queryOpt.Tags.Tags))
+	cp.Tags.Tags = append(cp.Tags.Tags, queryOpt.Tags.Tags...)
+
+	/* In case deeper copying Tags is needed
+	for _, tag := range queryOpt.Tags.Tags {
+		tagCopy := models.Tag{
+			Name:  make([]byte, len(tag.Name)),
+			Value:  make([]byte, len(tag.Value)),
+		}
+		copy(tagCopy.Name, tag.Name)
+		copy(tagCopy.Value, tag.Value)
+		cp.Tags.Tags = append(cp.Tags.Tags, tagCopy)
+	}
+	*/
+	return cp
+}
+
 func (p *promStorage) Write(ctx context.Context, query *storage.WriteQuery) error {
 	if query == nil {
 		return nil
 	}
-	if query.DuplicateWrite() {
+	if query.Options().DuplicateWrite {
+		// M3 call site may write the same data according to different storage policies.
+		// See downsampleAndWriter in src/cmd/services/m3coordinator/ingest/write.go
 		p.dupWrites.Inc(1)
 		return nil
 	}
+	if query.Options().FromIngestor {
+		// src/cmd/services/m3coordinator/ingest/m3msg/ingest.go reuses a WriteQuery object to write different
+		// time series by calling ResetWriteQuery(). We need to make a copy of the WriteQuery object to avoid
+		// race conditions.
+		p.ingestorWrites.Inc(1)
+		queryCopy, err := storage.NewWriteQuery(deepCopy(query.Options()))
+		if err != nil {
+			p.enqueueErr.Inc(1)
+			p.logger.Error("error copying write", zap.Error(err),
+				zap.String("write", query.String()))
+			return err
+		}
+		query = queryCopy
+	}
 	p.queryQueue <- query
 	p.enqueued.Inc(1)
-	if query.Attributes().MetricsType == storagemetadata.AggregatedMetricsType {
-		p.enqueuedAggr.Inc(1)
-	}
 	return nil
 }
 
@@ -324,6 +375,32 @@ func (p *promStorage) writeBatch(ctx context.Context, tenant tenantKey, queries 
 		p.logger.Debug("async write batch",
 			zap.String("tenant", string(tenant)),
 			zap.Int("size", len(queries)))
+	}
+	// TODO: remove this double check once the bug is confirmed to be fixed.
+	{
+		correctWq := make([]*storage.WriteQuery, 0, len(queries))
+		wrongTenants := 0
+		for _, wq := range queries {
+			correctTenant := p.getTenant(wq)
+			if correctTenant == tenant {
+				correctWq = append(correctWq, wq)
+			} else {
+				wrongTenants++
+				if rand.Float32() < 0.01 {
+					p.logger.Error("dropping a write because of a wrong tenant",
+						zap.String("expected_tenant", string(correctTenant)),
+						zap.String("actual_tenant", string(tenant)),
+						zap.String("time_series", wq.String()),
+						zap.Bool("from_ingestor", wq.Options().FromIngestor),
+					)
+				}
+			}
+		}
+		p.wrongTenant.Inc(int64(wrongTenants))
+		queries = correctWq
+	}
+	if len(queries) == 0 {
+		return nil
 	}
 	encoded, err := convertAndEncodeWriteQuery(queries)
 	if err != nil {
@@ -419,7 +496,7 @@ func (p *promStorage) doRequest(req *http.Request) (int, error) {
 	}
 	defer func() { _ = resp.Body.Close() }()
 	if resp.StatusCode/100 != 2 {
-		response, err := ioutil.ReadAll(resp.Body)
+		response, err := io.ReadAll(resp.Body)
 		if err != nil {
 			p.logger.Error("error reading body", zap.Error(err))
 			response = errorReadingBody
